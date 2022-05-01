40136f3f72a3150c5b9ee36f8c7ec726b98f8f6a
Ruben Salazar <94159087+RubenSalazar13@users.noreply.github.com>
2022-05-01 01:01:16 -0600

    Add files via upload

---
+++ schedule.php
@@ -0,0 +1,30 @@
+ <!DOCTYPE html>
+<html>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@300&family=Playfair+Display&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="#services">Services</a>
+  <a href="schedule.php">Schedule Service</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact Us</a>
+</div>
+<div class="coming-soon">
+  <center>
+    <h1>Coming Soon</h1>
+  </center>
+</div>
+        <footer>
+        <?php include('footer.php'); ?>
+        </footer>
+    </body>
+</html> 
