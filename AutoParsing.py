5e5633b69268ec9d37e5a3d762f7650d082b3b2b
Ruben Salazar <94159087+RubenSalazar13@users.noreply.github.com>
2022-05-16 08:12:28 -0600

    Update AutoParsing.py

--- AutoParsing.py
+++ AutoParsing.py
@@ -1,15 +1,18 @@
 import csv
 import MySQLdb
 
+#Database credentials
 mydb = MySQLdb.connect(host='localhost',
     user='root',
     passwd='PASSWORD',
     db='Test2')
 cursor = mydb.cursor()
 
+#input the file that you will attempt to extract infromtation from
 csv_data = csv.reader(open("Desktop/Python/BG.csv"))
 for row in csv_data:
 
+#The following has it set for three fields, but can adjust depending on fields    
     cursor.execute('INSERT INTO Vehicle(Vehicle_Make, \
           Vehicle_Year, Vehicle_Model)' \
           'VALUES("%s", "%s", "%s")', 
